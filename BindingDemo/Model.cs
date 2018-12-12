db832ea0efada6495754ac2a569395f33e47fff3
Artem Marakhovskyi <Artem.marakhovskyi@gmail.com>
2018-12-12 11:04:23 +0200

    First project is added. Binding. Console application

---
+++ BindingDemo/Model.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace BindingDemo.BareBinding
+{
+    public class Model : INotifyPropertyChanged
+    {
+        private readonly Timer _timer;
+
+        private int _percentage;
+        private DateTime _time = DateTime.Now;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int Percentage
+        {
+            get => _percentage;
+            set
+            {
+                if (value != _percentage)
+                {
+                    _percentage = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public DateTime Time
+        {
+            get => _time;
+            set
+            {
+                if (value != _time)
+                {
+                    _time = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public Model()
+        {
+            _timer = new Timer(Cycle, null, dueTime: 0, period: 10);
+        }
+
+        protected void OnPropertyChanged([CallerMemberName]string callerName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(callerName));
+        }
+
+        private void Cycle(object state)
+        {
+            Percentage = DateTime.Now.Millisecond / 10;
+            if (Percentage >= 90)
+            {
+                Time = DateTime.Now;
+                Percentage = 0;
+            }
+        }
+    }
+}
---
+++ BindingDemo/View.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace BindingDemo.BareBinding
+{
+    public class View
+    {
+        private const string OutputFormat = "{0}, {1}";
+
+        private readonly Model _model;
+
+        public View(Model model)
+        {
+            _model = model;
+            _model.PropertyChanged += ModelPropertyChanged;
+        }
+
+        private void ModelPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            switch (e.PropertyName)
+            {
+                case nameof(_model.Time):
+                    {
+                        Console.Clear();
+                        Out();
+                        break;
+                    }
+                case nameof(_model.Percentage):
+                    {
+                        Console.Clear();
+                        Out();
+                        break;
+                    }
+            }
+        }
+
+        private void Out()
+        {
+            Console.WriteLine(
+                string.Format(
+                    OutputFormat,
+                    _model.Time.ToString(),
+                    new string('.', _model.Percentage)));
+        }
+    }
+}
